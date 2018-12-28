b299d0d5b653290fcb7351342a4ae4b44e85b3e8
suowei <981199730@qq.com>
2018-12-28 22:26:13 +0800

    更新酷我音乐销量计算方法

--- szzj/models.py
+++ szzj/models.py
@@ -87,22 +87,17 @@ class Album(models.Model):
     def get_kuwo_sale_info(self):
         if self.kuwo_id is None:
             return
-        data = {'type': self.kuwo_id}
-        req = request.Request('http://vip1.kuwo.cn/fans/promotion/gettotalcnt', data=parse.urlencode(data).encode('utf-8'))
-        with request.urlopen(req) as f:
-            response = f.read().decode('utf-8')
-            json_data = json.loads(response)
-            self.kuwo_count = json_data['data']['total']
-            self.kuwo_money = Decimal(json_data['data']['totalMoney'])
-        if self.album_only:
-            return
         data = {'key': self.kuwo_id, 'op': 'gfc'}
         req = request.Request('http://vip1.kuwo.cn/fans/admin/sysInfo', data=parse.urlencode(data).encode('utf-8'))
         with request.urlopen(req) as f:
             response = f.read().decode('utf-8')
             json_data = json.loads(response)
             docs = json_data['fans']['docs']
+            self.kuwo_count = int(docs['total_cnt'])
             self.kuwo_song_count = int(docs['song_total_cnt'])
+            self.kuwo_money = self.price * self.kuwo_count
+            if not self.album_only:
+                self.kuwo_money += (self.kuwo_song_count - self.kuwo_count * self.song_num) * self.song_price
 
     def get_wyy_sale_info(self):
         if self.wyy_id is None:
