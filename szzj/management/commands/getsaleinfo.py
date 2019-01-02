be7d625630b305928beb6c95d25f21fddd48f4ac
suowei <suowei@users.noreply.github.com>
2019-01-02 15:36:47 +0800

    更新数据时一次获取全部专辑列表

--- szzj/management/commands/getsaleinfo.py
+++ szzj/management/commands/getsaleinfo.py
@@ -6,10 +6,12 @@ class Command(BaseCommand):
     help = 'Get sale data.'
 
     def handle(self, *args, **options):
-        i = Album.objects.count()
-        while i > 0:
-            album = Album.objects.get(id=i)
+        album_list = Album.objects.only('song_num', 'price', 'album_only', 'song_price',
+                                        'qq_id', 'kugou_id', 'kuwo_id', 'wyy_id', 'wyy_params', 'wyy_encSecKey',
+                                        'qq_count', 'qq_song_count', 'qq_money',
+                                        'kugou_count', 'kugou_song_count', 'kugou_money',
+                                        'kuwo_count', 'kuwo_song_count', 'kuwo_money',
+                                        'wyy_count', 'wyy_song_count', 'wyy_money', 'migu_money', 'money')
+        for album in album_list:
             album.get_sale_info()
             album.save()
-            i -= 1
-
