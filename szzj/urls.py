09f562d9500b0fefb427d961b0e3ec56a6d15bd4
suowei <suowei@users.noreply.github.com>
2019-01-07 17:34:03 +0800

    增加年份查询

--- szzj/urls.py
+++ szzj/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'szzj'
 urlpatterns = [
     path('', views.AlbumIndexView.as_view(), name='index'),
+    path('<int:year>/', views.year_index, name='year_index'),
     path('artist/', views.artist_index, name='artist_index'),
     path('artist/<int:pk>/', views.ArtistDetailView.as_view(), name='artist_detail'),
 ]
--- szzj/views.py
+++ szzj/views.py
@@ -15,6 +15,18 @@ class AlbumIndexView(generic.ListView):
                                                                               'wyy_count', 'wyy_song_count', 'wyy_money', 'money')
 
 
+def year_index(request, year):
+    album_list = Album.objects.filter(release_date__year=year).select_related('artist').order_by('-money').only('title', 'artist_id', 'artist__name',
+                                                                              'release_date', 'price', 'album_only',
+                                                                              'qq_id', 'kugou_id', 'kuwo_id', 'wyy_id',
+                                                                              'qq_count', 'qq_song_count', 'qq_money',
+                                                                              'kugou_count', 'kugou_song_count', 'kugou_money',
+                                                                              'kuwo_count', 'kuwo_song_count', 'kuwo_money',
+                                                                              'wyy_count', 'wyy_song_count', 'wyy_money', 'money')
+    context = {'year':year, 'album_list': album_list}
+    return render(request, 'szzj/album_list.html', context)
+
+
 class ArtistDetailView(generic.DetailView):
     model = Artist
 
