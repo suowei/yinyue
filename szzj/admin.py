7c44872fd4b2d33d8be0dcae25f738541c9e8cbd
suowei <981199730@qq.com>
2019-05-13 18:45:09 +0800

    将今日销量整合到销量获取命令中

--- szzj/admin.py
+++ szzj/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Artist
 from .models import Album
+from .models import AlbumData
 from .models import AlbumInfo
 from .models import Tour
 from .models import City
@@ -10,6 +11,7 @@ from .models import Concert
 
 admin.site.register(Artist)
 admin.site.register(Album)
+admin.site.register(AlbumData)
 admin.site.register(AlbumInfo)
 admin.site.register(Tour)
 admin.site.register(City)
