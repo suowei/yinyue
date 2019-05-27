2f1f5723dd96dbb54a0c6c477ba93b4b9db143ae
suowei <981199730@qq.com>
2019-05-27 08:37:18 +0800

    近期演唱会

--- szzj/views.py
+++ szzj/views.py
@@ -9,14 +9,19 @@ from .models import Artist, Album, AlbumData, AlbumInfo, Concert, Site
 def index(request):
     album_info_list = AlbumInfo.objects.select_related('artist').order_by('sale_time').only(
         'title', 'artist_id', 'artist__name', 'sale_time', 'info')
-    date = timezone.now().date() + timezone.timedelta(days=-30)
+    now = timezone.now()
+    date = now.date() + timezone.timedelta(days=-30)
     album_list = Album.objects.filter(release_date__gte=date).select_related('artist').order_by('-release_date').only(
         'title', 'artist_id', 'artist__name', 'release_date', 'price', 'album_only',
         'qq_id', 'kugou_id', 'kuwo_id', 'wyy_id', 'qq_count', 'qq_song_count', 'qq_money',
         'kugou_count', 'kugou_song_count', 'kugou_money', 'kuwo_count', 'kuwo_song_count', 'kuwo_money',
         'wyy_count', 'wyy_song_count', 'wyy_money', 'count', 'money')
     top_album_list = Album.objects.order_by('-money_today').filter(money_today__gt=0).select_related('artist')[:20]
-    context = {'album_list': album_list, 'album_info_list': album_info_list, 'top_album_list': top_album_list}
+    date = now + timezone.timedelta(days=7)
+    concert_list = Concert.objects.filter(date__gte=now, date__lt=date).select_related(
+        'tour', 'tour__artist', 'site', 'site__city').order_by('date')
+    context = {'album_list': album_list, 'album_info_list': album_info_list,
+               'top_album_list': top_album_list, 'concert_list': concert_list}
     return render(request, 'szzj/index.html', context)
 
 
