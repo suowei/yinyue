acfc2182f20e6c56664229a821df4d6c99b43741
suowei <981199730@qq.com>
2021-06-29 17:34:09 +0800

    管理页增加下拉框自动填充

--- szzj/admin.py
+++ szzj/admin.py
@@ -10,12 +10,48 @@ from .models import City
 from .models import Site
 from .models import Concert
 
-admin.site.register(Artist)
-admin.site.register(Album)
-admin.site.register(AlbumData)
+
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    autocomplete_fields = ['artist']
+
+
+class AlbumInfoAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['artist']
+
+
+class AlbumDataAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['album']
+
+
+class TourAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    autocomplete_fields = ['artist']
+
+
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class SiteAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    autocomplete_fields = ['city']
+
+
+class ConcertAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['tour', 'site']
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(AlbumData, AlbumDataAdmin)
 admin.site.register(AlbumDataDaily)
-admin.site.register(AlbumInfo)
-admin.site.register(Tour)
-admin.site.register(City)
-admin.site.register(Site)
-admin.site.register(Concert)
+admin.site.register(AlbumInfo, AlbumInfoAdmin)
+admin.site.register(Tour, TourAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Site, SiteAdmin)
+admin.site.register(Concert, ConcertAdmin)
