ea8eca00369635b3b81e839c3c08277128fc1bf5
suowei <981199730@qq.com>
2024-04-12 08:40:24 +0800

    关闭数字专辑功能

--- szzj/urls.py
+++ szzj/urls.py
@@ -1,27 +1,30 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'szzj'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('szzj/', views.album_index, name='szzj.index'),
-    path('szzj/<int:year>/', views.album_year_index, name='szzj.album_year_index'),
-    path('szzj/sales/', views.album_sales_index, name='szzj.album_sales_index'),
-    path('szzj/<int:year>/sales/', views.album_sales_year_index, name='szzj.album_sales_year_index'),
-    path('szzj/artist/', views.artist_index, name='szzj.artist_index'),
-    path('szzj/artist/<int:pk>/', views.ArtistDetailView.as_view(), name='szzj.artist_detail'),
-    path('szzj/new/', views.new_album_index, name='szzj.new_album_index'),
-    path('szzj/today', views.today_album_index, name='szzj.today_album_index'),
-    path('szzj/album/<int:pk>/', views.album_detail, name='szzj.album_detail'),
-    path('szzj/album/<int:album>/daily', views.album_data_daily, name='szzj.album_data_daily'),
-    path('szzj/album/<int:album>/<int:year>/<int:month>/<int:day>/', views.album_data_daily_detail, name='szzj.album_data_daily_detail'),
-    path('szzj/album/download/', views.album_download, name='szzj.album_download'),
+    path('', TemplateView.as_view(template_name="szzj/index.html")),
+    path('szzj/', TemplateView.as_view(template_name="szzj/index.html")),
+    # path('', views.index, name='index'),
+    # path('szzj/', views.album_index, name='szzj.index'),
+    # path('szzj/<int:year>/', views.album_year_index, name='szzj.album_year_index'),
+    # path('szzj/sales/', views.album_sales_index, name='szzj.album_sales_index'),
+    # path('szzj/<int:year>/sales/', views.album_sales_year_index, name='szzj.album_sales_year_index'),
+    # path('szzj/artist/', views.artist_index, name='szzj.artist_index'),
+    # path('szzj/artist/<int:pk>/', views.ArtistDetailView.as_view(), name='szzj.artist_detail'),
+    # path('szzj/new/', views.new_album_index, name='szzj.new_album_index'),
+    # path('szzj/today', views.today_album_index, name='szzj.today_album_index'),
+    # path('szzj/album/<int:pk>/', views.album_detail, name='szzj.album_detail'),
+    # path('szzj/album/<int:album>/daily', views.album_data_daily, name='szzj.album_data_daily'),
+    # path('szzj/album/<int:album>/<int:year>/<int:month>/<int:day>/', views.album_data_daily_detail, name='szzj.album_data_daily_detail'),
+    # path('szzj/album/download/', views.album_download, name='szzj.album_download'),
     path('yanchanghui/', views.ConcertIndexView.as_view(), name='yanchanghui.index'),
     path('yanchanghui/<int:year>/', views.concert_year_index, name='yanchanghui.concert_year_index'),
     path('yanchanghui/<int:year>/city/', views.concert_city_year_index, name='yanchanghui.concert_city_year_index'),
     path('yanchanghui/city/<int:pk>/', views.CityDetailView.as_view(), name='yanchanghui.city_detail'),
     path('yanchanghui/site/', views.SiteIndexView.as_view(), name='yanchanghui.site_index'),
     path('yanchanghui/site/<int:pk>/', views.SiteDetailView.as_view(), name='yanchanghui.site_detail'),
-    path('search/', views.search),
+    # path('search/', views.search),
 ]
